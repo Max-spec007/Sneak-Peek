4812bb7bbe46380ed3c528e731c03f221be8b4b1
Maxwell Kwarteng <maxtheflash007@gmail.com>
2021-03-11 20:19:19 -0500

    Added modified file to Sneak Peek Edited the ShoeList File along with the API call.

--- src/components/ShoeList/ShoeList.js
+++ src/components/ShoeList/ShoeList.js
@@ -9,14 +9,31 @@ class ShoeList extends React.Component {
   componentDidMount () {
     axios.get('https://api.thesneakerdatabase.com/v1/sneakers?limit=50')
       .then(res => {
-        console.log(res)
         this.setState({ sneakers: res.data })
+        // console.log('Hey', res, this.state.sneakers)
       })
   }
 
   render () {
-    return <ul>{this.state.sneakers.map(sneaker => <li key={sneaker.id}>{sneaker.name}</li>)}
-    </ul>
+    const { sneakers } = this.state
+    let shoes = ''
+    if (sneakers.length !== 0) {
+      shoes = (
+        sneakers.results.map(sneaker => <li key={sneaker.id}>{sneaker.brand}{sneaker.name}{sneaker.releaseDate}{sneaker.year}</li>)
+      )
+    } else {
+      shoes = '....Loading'
+    }
+
+    return (
+      <div>
+        <ul> {shoes}
+        </ul>
+      </div>
+    )
+
+    // return <ul>
+    // </ul>
   }
 }
 
